2ad918b6c0f60b3931488a6c184555356ce71946
Ryan <rfabester@gmail.com>
2014-01-02 13:08:24 -0800

    initial styling, file structure change to get compass to watch

---
+++ static/config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = ENV['STATIC_URL'] || '/static/'
+css_dir = "css"
+sass_dir = "css/src"
+images_dir = "img"
+javascripts_dir = "js"
+relative_assets = false
